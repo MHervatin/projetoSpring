306812a1989846092d88bf3a4b187e6f657e0090
Mateus Silva <hervatin.mateus@gmail.com>
2021-06-16 19:46:25 -0300

    Titulo: Teste de listagem API Rest
    Tipo: I (I - Implementação | C - Correção | M - Melhoria)
    
    Mensagem:
      - Implementado método para teste de listagem Rest.
    
    Observação:
    
    Referências:

--- src/main/java/com/msilva/cursoSpring/resources/CategoriaResource.java
+++ src/main/java/com/msilva/cursoSpring/resources/CategoriaResource.java
@@ -1,10 +1,14 @@
 package com.msilva.cursoSpring.resources;
 
+import com.msilva.cursoSpring.domain.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Classe responsável por controlar os métodos da API.
  *
  * @author Mateus
  */
@@ -12,8 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+    /**
+     * Teste de listagem de categorias.
+     *
+     * @return Uma lista com as categorias de teste.
+     */
     @GetMapping
-    public String listar() {
-        return "Rest está funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(Long.parseLong("1"), "Informática");
+        Categoria cat2 = new Categoria(Long.parseLong("2"), "Mercado");
+
+        ArrayList<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+
+        return categorias;
     }
 }
