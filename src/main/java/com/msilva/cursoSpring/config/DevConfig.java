54e06515cc88ff089b5a8bcaa8ec6fea1dd0d605
Mateus Silva <hervatin.mateus@gmail.com>
2021-07-21 21:09:40 -0300

    Titulo: Criando profile de desenvolvimento
    Tipo: I (I - Implementação | C - Correção | M - Melhoria)
    
    Mensagem:
      - Criado o profile de desenvolvimento e suas configurações.
    
    Observação:
    
    Referências:

---
+++ src/main/java/com/msilva/cursoSpring/config/DevConfig.java
@@ -0,0 +1,42 @@
+package com.msilva.cursoSpring.config;
+
+import com.msilva.cursoSpring.services.DBService;
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Configurações para o perfil de teste.
+ *
+ * @author Mateus
+ */
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    /**
+     * Efetua a instanciação dos dados de teste;
+     *
+     * @return True se os dados foram instânciados; caso contrário false;
+     *
+     * @throws ParseException
+     */
+    @Bean
+    public boolean instanciacaoBancoDeDados() throws ParseException {
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+
+        dbService.instanciarBancoDadosTeste();
+        return true;
+    }
+}
