434538dfd1baab2f6e297c7e29e09c620e8032f6
Mateus Silva <hervatin.mateus@gmail.com>
2021-07-26 19:36:27 -0300

    Titulo: Implementação básica das configurações de segurança
    Tipo: I (I - Implementação | C - Correção | M - Melhoria)
    
    Mensagem:
      - Implementada classe responsavél pela segurança da API.
    
      - Implementadas dependências necessarias do spring-security.
    
    Observação:
    
    Referências:

---
+++ src/main/java/com/msilva/cursoSpring/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.msilva.cursoSpring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * Classe responsável por efetuar configurações de segurança da aplicação.
+ *
+ * @author Mateus
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHERS = {};
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+        "/produtos/**",
+        "/categorias/**"};
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and().csrf().disable();
+
+        httpSecurity.authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .anyRequest().authenticated();
+
+        httpSecurity.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    /**
+     * Método responsavél por configurar o {@code Cors}.
+     *
+     * @return O {@code CorsConfigurationSource} configurado.
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource ubccs
+                = new UrlBasedCorsConfigurationSource();
+
+        ubccs.registerCorsConfiguration("/**",
+                new CorsConfiguration().applyPermitDefaultValues()
+        );
+
+        return ubccs;
+    }
+}
